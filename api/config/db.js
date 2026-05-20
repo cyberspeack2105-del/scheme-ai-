667f4699f96b8d4fd9539062b948bdf0b467dc97
cyberspeack2105-del <cyberspeack2105@gmail.com>
2026-05-20 19:20:14 +0530

    fix: hardcode MongoDB Atlas connection URL as requested

--- api/config/db.js
+++ api/config/db.js
@@ -2,35 +2,8 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    let mongoUrl = process.env.MONGODB_URL || process.env.MONGO_URL || 'mongodb://localhost:27017/auth_db';
-    
-    // Inject DB_NAME if specified and not already in connection string
-    if (process.env.DB_NAME) {
-      const dbName = process.env.DB_NAME;
-      if (mongoUrl.includes('mongodb+srv://')) {
-        // If it ends with '/' or has '?' we need to insert DB_NAME before '?'
-        if (mongoUrl.includes('?')) {
-          // Check if DB_NAME is already there, if not insert it
-          const urlParts = mongoUrl.split('?');
-          if (!urlParts[0].endsWith(`/${dbName}`)) {
-            if (urlParts[0].endsWith('/')) {
-              urlParts[0] = urlParts[0] + dbName;
-            } else {
-              urlParts[0] = urlParts[0] + '/' + dbName;
-            }
-            mongoUrl = urlParts.join('?');
-          }
-        } else {
-          if (!mongoUrl.endsWith(`/${dbName}`)) {
-            if (mongoUrl.endsWith('/')) {
-              mongoUrl = mongoUrl + dbName;
-            } else {
-              mongoUrl = mongoUrl + '/' + dbName;
-            }
-          }
-        }
-      }
-    }
+    // Use only the MongoDB Atlas URL directly as requested
+    const mongoUrl = 'mongodb+srv://raju:kanthalloor123@cluster0.dsttruk.mongodb.net/kanthalloor_db?appName=Cluster0';
 
     await mongoose.connect(mongoUrl, {
       serverSelectionTimeoutMS: 5000,
