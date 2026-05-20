b2e4afcfe3234352b7f1d1b74273bc3af73bf34c
cyberspeack2105-del <cyberspeack2105@gmail.com>
2026-05-20 11:37:44 +0530

    clean: remove obsolete python and html backend/frontend folders and root diagrams

--- login/backend/test_login.py
+++
@@ -1,9 +0,0 @@
-from auth import login_user
-
-print("🔑 LOGIN TEST")
-
-username = "raju"
-password = "123456"
-
-result = login_user(username, password)
-print(result)
--- login/backend/test_register.py
+++
@@ -1,9 +0,0 @@
-from auth import register_user
-
-print("🔐 REGISTER TEST")
-
-username = "raju"
-password = "123456"
-
-result = register_user(username, password)
-print(result)
